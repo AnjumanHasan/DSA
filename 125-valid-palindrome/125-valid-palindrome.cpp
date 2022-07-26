efa12f5ec075a9f23e73b163be7f41258ab297ef
Anjuman Hasan <82674743+AnjumanHasan@users.noreply.github.com>
2022-07-27 00:08:15 +0530

    Time: 7 ms (69.18%), Space: 7.6 MB (45.60%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp="";
+        if(s==" ")
+        {
+            return true;
+        }
+        if(s.length()==1)
+        {
+            return true;
+        }
+        
+        for(int i=0;i<s.length();i++)
+        {
+            
+            if(s[i]>=65 && s[i]<=90)
+            {
+             temp+=s[i]+32;  
+            }
+            else if(s[i]>=97 && s[i]<=122)
+            {
+                temp+=s[i];
+            }
+            else if(s[i]>='0' && s[i]<='9')
+            {
+                temp+=s[i];
+            }
+        }
+    
+        int start=0,end=temp.size()-1;
+        if(end==start)
+        {
+            return true;
+        }
+        while(end>start)
+        {
+            if(temp[start]!=temp[end])
+            {
+                return false;
+            }
+            end--;
+            start++;
+        }
+        return true;
+    }
+};
