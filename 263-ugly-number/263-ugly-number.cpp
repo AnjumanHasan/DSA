39c88b1884e1ee091040c8c5514eb80fddc4cfd1
Anjuman Hasan <82674743+AnjumanHasan@users.noreply.github.com>
2022-09-07 21:16:43 +0530

    Time: 4 ms (41.20%), Space: 5.8 MB (87.51%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        vector<int> v;
+        if(n==0)
+        {
+            return false;
+        }
+        while(n%2==0)
+        {
+            n=n/2;
+        }
+        while(n%3==0)
+        {
+            n=n/3;
+        }
+        while(n%5==0)
+        {
+            n=n/5;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return false;
+    }
+};
