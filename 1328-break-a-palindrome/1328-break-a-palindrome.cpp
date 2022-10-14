b86c5d1c1623014bad678c0154652d7037ea8bfa
Anjuman Hasan <82674743+AnjumanHasan@users.noreply.github.com>
2022-10-15 00:35:00 +0530

    Time: 7 ms (9.00%), Space: 6.3 MB (44.97%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -5,21 +5,16 @@ public:
         {
             return "";
         }
-        bool flag=true;
         for(int i=0;i<palindrome.size()/2;i++)
         {
             if(palindrome[i]!='a')
             {
                 palindrome[i]='a';
-                flag=false;
-                break;
+                return palindrome;
             }
         }
-        if(flag)
-        {
+        
             palindrome[palindrome.size()-1]='b';
-        }
-            
         return palindrome;
     }
 };
